ea249c911f5c62269c945393d3cafcf39295eb12
Akarsh Jain <akarsh.jain.790@gmail.com>
2024-03-26 00:32:53 +0530

    settings: Add initial implementation for custom welcome bot message.
    This commit extends the functionality of the Welcome Bot to
    include the custom welcome message configured by administrators. It
    ensures that new users receive the additional custom message along
    with the standard welcome message upon joining the organization.
    
    Fixes: #27663

--- web/tests/settings_org.test.js
+++ web/tests/settings_org.test.js
@@ -624,6 +624,9 @@ test("set_up", ({override, override_rewire}) => {
     );
     $("#enable_digest_emails_label").set_parent($.create("<stub digest setting checkbox>"));
     $("#id_realm_digest_weekday").set_parent($.create("<stub digest weekday setting dropdown>"));
+    $("#id_realm_custom_welcome_message_text").set_parent(
+        $.create("<stub custom welcome message text>"),
+    );
     $("#allowed_domains_label").set_parent($.create("<stub-allowed-domain-label-parent>"));
     const $waiting_period_parent_elem = $.create("waiting-period-parent-stub");
     $("#id_realm_waiting_period_threshold").set_parent($waiting_period_parent_elem);
